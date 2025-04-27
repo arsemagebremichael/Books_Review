0ce30dd984e0ffff05f187d9f4c1d4db07b8b372
A <Arsema.arsemaagebremichael@gmail.com>
2025-04-27 14:38:45 +0300

    adding files

---
+++ index.js
@@ -0,0 +1,24 @@
+function redirect() {
+    window.location.href = 'https://www.sackett.net/Homegoing.pdf';
+  }
+
+function homegoing(){
+  window.location.href = 'home_going.html';
+
+}
+
+function bornacrime(){
+  window.location.href = 'trevor_noah.html';
+
+}
+
+
+function who_moved(){
+  window.location.href = 'https://medium.com/@joaomoliveira.gomes/who-moved-my-cheese-by-dr-spencer-johnson-book-review-f1adba4ecec3';
+
+}
+
+function readNow(){
+  window.location.href = 'https://pursuitofdiversity.wordpress.com/wp-content/uploads/2018/06/born-a-crime-trevor-noah.pdf';
+
+}
